9337718749dedd2270f4728474dfeb86e86935ee
Cédric Ronvel <cedric.ronvel@gmail.com>
2016-01-04 14:03:54 +0100

    .removeItems()

--- bdd-spec.md
+++ bdd-spec.md
@@ -1,12 +1,46 @@
 # TOC
-   - [...](#)
+   - [.removeItems()](#removeitems)
 <a name=""></a>
  
-<a name=""></a>
-# ...
-....
+<a name="removeitems"></a>
+# .removeItems()
+should produce a new array without any element matching the second array.
 
 ```js
+var input , copy ;
 
+expect( array.removeItems( [ 1,2,3,4,5,4,3,2,1 ] , [ 2,4 ] ) ).to.eql( [ 1,3,5,3,1 ] ) ;
+expect( array.removeItems( [ 1,2,3,4,5,4,3,2,1 ] , [ 2,4,6 ] ) ).to.eql( [ 1,3,5,3,1 ] ) ;
+expect( array.removeItems( [ 1,2,3,4,5,4,3,2,1 ] , [] ) ).to.eql( [ 1,2,3,4,5,4,3,2,1 ] ) ;
+expect( array.removeItems( [] , [ 2,4 ] ) ).to.eql( [] ) ;
+
+var a = { a: 'a' } ;
+var b = { b: 'b' } ;
+var c = { c: 'c' } ;
+
+// Check if the src/input array was left untouched
+input = [ a , a , b , a ] ;
+copy = input ;
+expect( array.removeItems( input , [ a ] ) ).to.eql( [ b ] ) ;
+expect( input ).to.eql( [ a , a , b , a ] ) ;
+expect( input ).to.be( copy ) ;
+
+expect( array.removeItems( [ a , a , a ] , [ a ] ) ).to.eql( [] ) ;
+expect( array.removeItems( [ a , a , b , a ] , [ a ] ) ).to.eql( [ b ] ) ;
+expect( array.removeItems( [ a , a , b , a ] , [ c ] ) ).to.eql( [ a , a , b , a ] ) ;
+expect( array.removeItems( [ a , a , b , a ] , [ a , c ] ) ).to.eql( [ b ] ) ;
+
+expect( array.removeItems( [ a , b , null , c ] , [ b ] ) ).to.eql( [ a , null , c ] ) ;
+expect( array.removeItems( [ a , b , c ] , [ b , null ] ) ).to.eql( [ a , c ] ) ;
+expect( array.removeItems( [ a , null , b , c ] , [ b , null ] ) ).to.eql( [ a , c ] ) ;
+
+expect( array.removeItems( [ a , b , undefined , c ] , [ b ] ) ).to.eql( [ a , undefined , c ] ) ;
+expect( array.removeItems( [ a , b , c ] , [ b , undefined ] ) ).to.eql( [ a , c ] ) ;
+expect( array.removeItems( [ a , undefined , b , c ] , [ b , undefined ] ) ).to.eql( [ a , c ] ) ;
+
+expect( array.removeItems( [ null , a , b , undefined , c ] , [ b ] ) ).to.eql( [ null , a , undefined , c ] ) ;
+expect( array.removeItems( [ null , a , b , undefined , c ] , [ null , b ] ) ).to.eql( [ a , undefined , c ] ) ;
+expect( array.removeItems( [ null , a , b , undefined , c ] , [ b , undefined ] ) ).to.eql( [ null , a , c ] ) ;
+expect( array.removeItems( [ null , a , b , undefined , c ] , [ undefined , null , b ] ) ).to.eql( [ a , c ] ) ;
 ```
 
--- test/array-test.js
+++ test/array-test.js
@@ -1,7 +1,7 @@
 /*
 	The Cedric's Swiss Knife (CSK) - CSK array toolbox test suite
 
-	Copyright (c) 2014 Cédric Ronvel 
+	Copyright (c) 2014-2016 Cédric Ronvel 
 	
 	The MIT License (MIT)
 
@@ -39,9 +39,44 @@ var expect = require( 'expect.js' ) ;
 
 
 
-describe( "..." , function() {
+describe( ".removeItems()" , function() {
 	
-	it( "..." , function() {
+	it( "should produce a new array without any element matching the second array" , function() {
+		var input , copy ;
+		
+		expect( array.removeItems( [ 1,2,3,4,5,4,3,2,1 ] , [ 2,4 ] ) ).to.eql( [ 1,3,5,3,1 ] ) ;
+		expect( array.removeItems( [ 1,2,3,4,5,4,3,2,1 ] , [ 2,4,6 ] ) ).to.eql( [ 1,3,5,3,1 ] ) ;
+		expect( array.removeItems( [ 1,2,3,4,5,4,3,2,1 ] , [] ) ).to.eql( [ 1,2,3,4,5,4,3,2,1 ] ) ;
+		expect( array.removeItems( [] , [ 2,4 ] ) ).to.eql( [] ) ;
+		
+		var a = { a: 'a' } ;
+		var b = { b: 'b' } ;
+		var c = { c: 'c' } ;
+		
+		// Check if the src/input array was left untouched
+		input = [ a , a , b , a ] ;
+		copy = input ;
+		expect( array.removeItems( input , [ a ] ) ).to.eql( [ b ] ) ;
+		expect( input ).to.eql( [ a , a , b , a ] ) ;
+		expect( input ).to.be( copy ) ;
+		
+		expect( array.removeItems( [ a , a , a ] , [ a ] ) ).to.eql( [] ) ;
+		expect( array.removeItems( [ a , a , b , a ] , [ a ] ) ).to.eql( [ b ] ) ;
+		expect( array.removeItems( [ a , a , b , a ] , [ c ] ) ).to.eql( [ a , a , b , a ] ) ;
+		expect( array.removeItems( [ a , a , b , a ] , [ a , c ] ) ).to.eql( [ b ] ) ;
+		
+		expect( array.removeItems( [ a , b , null , c ] , [ b ] ) ).to.eql( [ a , null , c ] ) ;
+		expect( array.removeItems( [ a , b , c ] , [ b , null ] ) ).to.eql( [ a , c ] ) ;
+		expect( array.removeItems( [ a , null , b , c ] , [ b , null ] ) ).to.eql( [ a , c ] ) ;
+		
+		expect( array.removeItems( [ a , b , undefined , c ] , [ b ] ) ).to.eql( [ a , undefined , c ] ) ;
+		expect( array.removeItems( [ a , b , c ] , [ b , undefined ] ) ).to.eql( [ a , c ] ) ;
+		expect( array.removeItems( [ a , undefined , b , c ] , [ b , undefined ] ) ).to.eql( [ a , c ] ) ;
+		
+		expect( array.removeItems( [ null , a , b , undefined , c ] , [ b ] ) ).to.eql( [ null , a , undefined , c ] ) ;
+		expect( array.removeItems( [ null , a , b , undefined , c ] , [ null , b ] ) ).to.eql( [ a , undefined , c ] ) ;
+		expect( array.removeItems( [ null , a , b , undefined , c ] , [ b , undefined ] ) ).to.eql( [ null , a , c ] ) ;
+		expect( array.removeItems( [ null , a , b , undefined , c ] , [ undefined , null , b ] ) ).to.eql( [ a , c ] ) ;
 	} ) ;
 	
 } ) ;
