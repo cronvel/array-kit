ca979d17a8a8a87e2c3fe65745bdf3f6c34c0a72
Cédric Ronvel <cedric.ronvel@gmail.com>
2017-01-24 11:24:36 +0100

    unit test for .delete()

--- test/array-test.js
+++ test/array-test.js
@@ -28,7 +28,7 @@
 /* global describe, it, before, after */
 
 
-var array = require( '../lib/array.js' ) ;
+var arrayKit = require( '../lib/array-kit.js' ) ;
 var expect = require( 'expect.js' ) ;
 
 
@@ -44,10 +44,10 @@ describe( ".removeItems()" , function() {
 	it( "should produce a new array without any element matching the second array" , function() {
 		var input , copy ;
 		
-		expect( array.removeItems( [ 1,2,3,4,5,4,3,2,1 ] , [ 2,4 ] ) ).to.eql( [ 1,3,5,3,1 ] ) ;
-		expect( array.removeItems( [ 1,2,3,4,5,4,3,2,1 ] , [ 2,4,6 ] ) ).to.eql( [ 1,3,5,3,1 ] ) ;
-		expect( array.removeItems( [ 1,2,3,4,5,4,3,2,1 ] , [] ) ).to.eql( [ 1,2,3,4,5,4,3,2,1 ] ) ;
-		expect( array.removeItems( [] , [ 2,4 ] ) ).to.eql( [] ) ;
+		expect( arrayKit.removeItems( [ 1,2,3,4,5,4,3,2,1 ] , [ 2,4 ] ) ).to.eql( [ 1,3,5,3,1 ] ) ;
+		expect( arrayKit.removeItems( [ 1,2,3,4,5,4,3,2,1 ] , [ 2,4,6 ] ) ).to.eql( [ 1,3,5,3,1 ] ) ;
+		expect( arrayKit.removeItems( [ 1,2,3,4,5,4,3,2,1 ] , [] ) ).to.eql( [ 1,2,3,4,5,4,3,2,1 ] ) ;
+		expect( arrayKit.removeItems( [] , [ 2,4 ] ) ).to.eql( [] ) ;
 		
 		var a = { a: 'a' } ;
 		var b = { b: 'b' } ;
@@ -56,31 +56,67 @@ describe( ".removeItems()" , function() {
 		// Check if the src/input array was left untouched
 		input = [ a , a , b , a ] ;
 		copy = input ;
-		expect( array.removeItems( input , [ a ] ) ).to.eql( [ b ] ) ;
+		expect( arrayKit.removeItems( input , [ a ] ) ).to.eql( [ b ] ) ;
 		expect( input ).to.eql( [ a , a , b , a ] ) ;
 		expect( input ).to.be( copy ) ;
 		
-		expect( array.removeItems( [ a , a , a ] , [ a ] ) ).to.eql( [] ) ;
-		expect( array.removeItems( [ a , a , b , a ] , [ a ] ) ).to.eql( [ b ] ) ;
-		expect( array.removeItems( [ a , a , b , a ] , [ c ] ) ).to.eql( [ a , a , b , a ] ) ;
-		expect( array.removeItems( [ a , a , b , a ] , [ a , c ] ) ).to.eql( [ b ] ) ;
+		expect( arrayKit.removeItems( [ a , a , a ] , [ a ] ) ).to.eql( [] ) ;
+		expect( arrayKit.removeItems( [ a , a , b , a ] , [ a ] ) ).to.eql( [ b ] ) ;
+		expect( arrayKit.removeItems( [ a , a , b , a ] , [ c ] ) ).to.eql( [ a , a , b , a ] ) ;
+		expect( arrayKit.removeItems( [ a , a , b , a ] , [ a , c ] ) ).to.eql( [ b ] ) ;
 		
-		expect( array.removeItems( [ a , b , null , c ] , [ b ] ) ).to.eql( [ a , null , c ] ) ;
-		expect( array.removeItems( [ a , b , c ] , [ b , null ] ) ).to.eql( [ a , c ] ) ;
-		expect( array.removeItems( [ a , null , b , c ] , [ b , null ] ) ).to.eql( [ a , c ] ) ;
+		expect( arrayKit.removeItems( [ a , b , null , c ] , [ b ] ) ).to.eql( [ a , null , c ] ) ;
+		expect( arrayKit.removeItems( [ a , b , c ] , [ b , null ] ) ).to.eql( [ a , c ] ) ;
+		expect( arrayKit.removeItems( [ a , null , b , c ] , [ b , null ] ) ).to.eql( [ a , c ] ) ;
 		
-		expect( array.removeItems( [ a , b , undefined , c ] , [ b ] ) ).to.eql( [ a , undefined , c ] ) ;
-		expect( array.removeItems( [ a , b , c ] , [ b , undefined ] ) ).to.eql( [ a , c ] ) ;
-		expect( array.removeItems( [ a , undefined , b , c ] , [ b , undefined ] ) ).to.eql( [ a , c ] ) ;
+		expect( arrayKit.removeItems( [ a , b , undefined , c ] , [ b ] ) ).to.eql( [ a , undefined , c ] ) ;
+		expect( arrayKit.removeItems( [ a , b , c ] , [ b , undefined ] ) ).to.eql( [ a , c ] ) ;
+		expect( arrayKit.removeItems( [ a , undefined , b , c ] , [ b , undefined ] ) ).to.eql( [ a , c ] ) ;
 		
-		expect( array.removeItems( [ null , a , b , undefined , c ] , [ b ] ) ).to.eql( [ null , a , undefined , c ] ) ;
-		expect( array.removeItems( [ null , a , b , undefined , c ] , [ null , b ] ) ).to.eql( [ a , undefined , c ] ) ;
-		expect( array.removeItems( [ null , a , b , undefined , c ] , [ b , undefined ] ) ).to.eql( [ null , a , c ] ) ;
-		expect( array.removeItems( [ null , a , b , undefined , c ] , [ undefined , null , b ] ) ).to.eql( [ a , c ] ) ;
+		expect( arrayKit.removeItems( [ null , a , b , undefined , c ] , [ b ] ) ).to.eql( [ null , a , undefined , c ] ) ;
+		expect( arrayKit.removeItems( [ null , a , b , undefined , c ] , [ null , b ] ) ).to.eql( [ a , undefined , c ] ) ;
+		expect( arrayKit.removeItems( [ null , a , b , undefined , c ] , [ b , undefined ] ) ).to.eql( [ null , a , c ] ) ;
+		expect( arrayKit.removeItems( [ null , a , b , undefined , c ] , [ undefined , null , b ] ) ).to.eql( [ a , c ] ) ;
 	} ) ;
 	
 } ) ;
 
+
+
+describe( ".delete()" , function() {
+	
+	it( "should delete one index in-place" , function() {
+		var array
+		
+		array = [ 1,2,3,4,5 ] ;
+		arrayKit.delete( array , 7 ) ;
+		expect( array ).to.eql( [ 1,2,3,4,5 ] ) ;
+		arrayKit.delete( array , 6 ) ;
+		expect( array ).to.eql( [ 1,2,3,4,5 ] ) ;
+		arrayKit.delete( array , 5 ) ;
+		expect( array ).to.eql( [ 1,2,3,4,5 ] ) ;
+		
+		array = [ 1,2,3,4,5 ] ;
+		arrayKit.delete( array , 0 ) ;
+		expect( array ).to.eql( [ 2,3,4,5 ] ) ;
+		
+		array = [ 1,2,3,4,5 ] ;
+		arrayKit.delete( array , 1 ) ;
+		expect( array ).to.eql( [ 1,3,4,5 ] ) ;
+		
+		array = [ 1,2,3,4,5 ] ;
+		arrayKit.delete( array , 2 ) ;
+		expect( array ).to.eql( [ 1,2,4,5 ] ) ;
+		
+		array = [ 1,2,3,4,5 ] ;
+		arrayKit.delete( array , 3 ) ;
+		expect( array ).to.eql( [ 1,2,3,5 ] ) ;
+		
+		array = [ 1,2,3,4,5 ] ;
+		arrayKit.delete( array , 4 ) ;
+		expect( array ).to.eql( [ 1,2,3,4 ] ) ;
+	} ) ;
+} ) ;
 
 
 
